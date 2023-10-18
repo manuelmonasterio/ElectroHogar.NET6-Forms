d4ec1441db775cd2df3fc05181ad78d786e78560
PGerenni <patriciogerenni@gmail.com>
2023-10-18 12:27:01 -0300

    BajaProducto
    Inicio Baja producto y creo clase Producto Negocio

---
+++ Presentacion/BajaProducto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class BajaProducto
+{
+    private static ValidacionesDatos vd = new ValidacionesDatos();
+	private static ProductosNegocio pn = new ProductosNegocio();
+
+    public Productos DarBajaProducto()
+	{
+		string idProducto;
+		bool flag;
+
+		do
+		{
+
+			Console.Write("Ingrese el Id del producto a dar de baja: ");
+			idProducto = Console.Readline();
+			flag = vd.ValidarVacio(idProducto, "ID Producto");
+			flag = vd.ValidarID(idProducto);
+
+		} while (flag == false);
+
+        Menu menu = new Menu();
+		Productos producto = pn.BuscarProducto(idProducto);
+		return producto;
+    }
+}
